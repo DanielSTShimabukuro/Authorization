367e7970f24ad1daac575c30c66e4eb733e4cc80
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-01-12 20:03:00 -0300

    feat(UserDetailsImpl): add getUsername function

--- src/main/java/authorization/authorization/entities/user/UserDetailsImpl.java
+++ src/main/java/authorization/authorization/entities/user/UserDetailsImpl.java
@@ -33,8 +33,8 @@ public class UserDetailsImpl implements UserDetails {
 
   @Override
   public String getUsername() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
+    return this.user
+                .getUsername();
   }
 
   
